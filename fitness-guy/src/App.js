64a04b2bce99faaeced199ce4da168b288a957b7
Danger <51889207+DangerDan88@users.noreply.github.com>
2020-06-17 13:26:03 -0600

    added tailwind css to this base start test project for a fitness app

---
+++ fitness-guy/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./assets/main.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-green-500">here</header>
+    </div>
+  );
+}
+
+export default App;
